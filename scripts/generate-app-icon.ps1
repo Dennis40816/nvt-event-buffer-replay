4d4220f19d82fd8e4a7e4d8b84cb000a4abc88b9
Dennis Liu <dennis48161025@gmail.com>
2026-08-21 00:42:39 +0800

    fix: integrate replay mark into application icon

--- scripts/generate-app-icon.ps1
+++ scripts/generate-app-icon.ps1
@@ -43,21 +43,51 @@ foreach ($size in $sizes) {
             $borderPen.Dispose()
         }
 
-        $tracePen = [System.Drawing.Pen]::new([System.Drawing.ColorTranslator]::FromHtml('#B9E653'), [Math]::Max(1.5, 5.5 * $scale))
+        $traceColor = [System.Drawing.ColorTranslator]::FromHtml('#69D7E5')
+        $accentColor = [System.Drawing.ColorTranslator]::FromHtml('#B9E653')
+        $tracePen = [System.Drawing.Pen]::new($traceColor, [Math]::Max(1.5, 5.2 * $scale))
         try {
             $tracePen.StartCap = [System.Drawing.Drawing2D.LineCap]::Round
             $tracePen.EndCap = [System.Drawing.Drawing2D.LineCap]::Round
             $tracePen.LineJoin = [System.Drawing.Drawing2D.LineJoin]::Round
             $points = [System.Drawing.PointF[]]@(
-                [System.Drawing.PointF]::new(17 * $scale, 45 * $scale),
-                [System.Drawing.PointF]::new(17 * $scale, 19 * $scale),
-                [System.Drawing.PointF]::new(47 * $scale, 45 * $scale),
-                [System.Drawing.PointF]::new(47 * $scale, 19 * $scale)
+                [System.Drawing.PointF]::new(14 * $scale, 44 * $scale),
+                [System.Drawing.PointF]::new(14 * $scale, 20 * $scale),
+                [System.Drawing.PointF]::new(36 * $scale, 43 * $scale)
             )
             $graphics.DrawLines($tracePen, $points)
         }
         finally {
             $tracePen.Dispose()
+        }
+
+        $playPath = [System.Drawing.Drawing2D.GraphicsPath]::new()
+        $playBrush = [System.Drawing.SolidBrush]::new($accentColor)
+        try {
+            $playPath.StartFigure()
+            $playPath.AddBezier(
+                38 * $scale, 20.5 * $scale,
+                36.7 * $scale, 19.7 * $scale,
+                35.5 * $scale, 20.5 * $scale,
+                35.5 * $scale, 22 * $scale)
+            $playPath.AddLine(35.5 * $scale, 22 * $scale, 35.5 * $scale, 42 * $scale)
+            $playPath.AddBezier(
+                35.5 * $scale, 42 * $scale,
+                35.5 * $scale, 43.6 * $scale,
+                37.2 * $scale, 44.4 * $scale,
+                38.5 * $scale, 43.5 * $scale)
+            $playPath.AddLine(38.5 * $scale, 43.5 * $scale, 54.3 * $scale, 33.8 * $scale)
+            $playPath.AddBezier(
+                54.3 * $scale, 33.8 * $scale,
+                55.7 * $scale, 33 * $scale,
+                55.7 * $scale, 31 * $scale,
+                54.3 * $scale, 30.2 * $scale)
+            $playPath.CloseFigure()
+            $graphics.FillPath($playBrush, $playPath)
+        }
+        finally {
+            $playPath.Dispose()
+            $playBrush.Dispose()
         }
 
         $stream = [System.IO.MemoryStream]::new()
